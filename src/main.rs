5341043aea572040d29b10586608a2d79fbdb1d5
Present Wenhao <meinnameistvater@gmail.com>
2022-04-25 23:15:09 +0800

    learn how to use struct

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
-
-fn main() {
-    let user1 = User {
-        email : String::from("meinnameistvater@gmail.com"),
-        username : String::from("PresentWH"),
-        active : true,
-        sign_in_count : 1,
-    };
-
-    let user2 = User {
-        active : user1.active,
-        username : user1.username,
-        email : String::from("1603554587@qq.com"),
-        sign_in_count : user1.sign_in_count,
-    };
-
-    let user3 = User {
-        email : String::from("Xavier72bit@wjx.com"),
-        username : String::from("WJX"),
-        ..user1
-    };
-    
-    println!("User2: ");
-    println!("{}",user2.username);
-    println!("{}",user2.email);
-    println!("User3: ");
-    println!("{}",user3.username);
-    println!("{}",user3.email);
-
+#[derive(Debug)]
+struct Rectangle {
+    width : u32,
+    height : u32,
 }
 
-struct User {
-    active : bool,
-    username : String,
-    email : String,
-    sign_in_count : u64,
+fn main() {
+    let rect1 = Rectangle {
+        width : 30,
+        height : 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+    println!("rect1 is {:#?}",rect1);
+}
+
+fn area(rectangle:&Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
